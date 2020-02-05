8a783ac799b2bb20b56cc3a9d452ff3b16c201c4
sssh <ssshobeiry@gmail.com>
2020-02-05 10:05:24 +0330

    add to pip

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from Jalalian import Date, jdate
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='shobeiry-jalali',
+    packages=['shobeiry-jalali'],
+    version='1.0',
+    license='MIT',
+    description='This Package Helps Developers To Easily Work With Jalali (Shamsi Or Iranian) Dates In Python applications.',
+    author='seyed sadeq shobeiri',
+    author_email='ssshobeiry@gmail.com',
+    url='https://github.com/shobeiry/jalali',
+    download_url='https://github.com/shobeiry/jalali/archive/master.zip',
+    keywords=['Jalali', 'python', 'sssh', 'shobeiry', 'jalali date'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
